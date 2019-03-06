ec33b3a115af96ea4b16a98ab799dcdb8ad51d65
Lukas Grenstad <lukasgrenstad@gmail.com>
2019-03-06 11:31:34 +0100

    ..

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
-import * as React from "react";
-import { Component } from "react-simplified";
-import ReactDOM from "react-dom";
-import { NavLink, HashRouter, Route } from "react-router-dom";
-import { studentService } from "./services";
-import { Card, List, Row, Column, NavBar, Button, Form } from "./widgets";
+import * as React from 'react';
+import { Component } from 'react-simplified';
+import ReactDOM from 'react-dom';
+import { NavLink, HashRouter, Route } from 'react-router-dom';
+import { studentService } from './services';
+import { Card, List, Row, Column, NavBar, Button, Form } from './widgets';
 
-import createHashHistory from "history/createHashHistory";
+import createHashHistory from 'history/createHashHistory';
 const history = createHashHistory(); // Use history.push(...) to programmatically change path, for instance after successfully saving a student
 
 class Menu extends Component {
   render() {
     return (
-      <NavBar brand="WhiteBoard">
-        <NavBar.Link to="/students">Students</NavBar.Link>
-        <NavBar.Link to="/newstudent">New Student</NavBar.Link>
-        <NavBar.Link to="/subjects">Subjects</NavBar.Link>
-        <NavBar.Link to="/newsubject">New Subject</NavBar.Link>
+      <NavBar brand="AS Sykkelutleie">
+        <NavBar.Link to="/students">Bookings</NavBar.Link>
+        <NavBar.Link to="/newstudent">Customers</NavBar.Link>
+        <NavBar.Link to="/subjects">Bikes</NavBar.Link>
+        <NavBar.Link to="/newsubject">Maintenance</NavBar.Link>
+        <NavBar.Link to="/newsubject">Transportation</NavBar.Link>
+        <NavBar.Link to="/newsubject">Reports</NavBar.Link>
       </NavBar>
     );
   }
@@ -23,7 +25,7 @@ class Menu extends Component {
 
 class Home extends Component {
   render() {
-    return <Card title="Welcome">Welcome to WhiteBoard</Card>;
+    return <Card title="AS Sykkelutleie">Velkommen til AS Sykkelutleie</Card>;
   }
 }
 
@@ -35,7 +37,7 @@ class StudentList extends Component {
       <Card title="Students">
         <List>
           {this.students.map(student => (
-            <List.Item key={student.id} to={"/students/" + student.id}>
+            <List.Item key={student.id} to={'/students/' + student.id}>
               {student.name}
             </List.Item>
           ))}
@@ -81,7 +83,7 @@ class StudentDetails extends Component {
   }
 
   edit() {
-    history.push("/students/" + this.student.id + "/edit");
+    history.push('/students/' + this.student.id + '/edit');
   }
 }
 
@@ -95,17 +97,9 @@ class StudentEdit extends Component {
       <div>
         <Card title="Edit student">
           <Form.Label>Name:</Form.Label>
-          <Form.Input
-            type="text"
-            value={this.student.name}
-            onChange={e => (this.student.name = e.target.value)}
-          />
+          <Form.Input type="text" value={this.student.name} onChange={e => (this.student.name = e.target.value)} />
           <Form.Label>Email:</Form.Label>
-          <Form.Input
-            type="text"
-            value={this.student.email}
-            onChange={e => (this.student.email = e.target.value)}
-          />
+          <Form.Input type="text" value={this.student.email} onChange={e => (this.student.email = e.target.value)} />
         </Card>
         <Row>
           <Column>
@@ -127,35 +121,27 @@ class StudentEdit extends Component {
 
   save() {
     studentService.updateStudent(this.student, () => {
-      history.push("/students/" + this.props.match.params.id);
+      history.push('/students/' + this.props.match.params.id);
     });
   }
 
   cancel() {
-    history.push("/students/" + this.props.match.params.id);
+    history.push('/students/' + this.props.match.params.id);
   }
 }
 
 class StudentNew extends Component {
-  name = "";
-  email = "";
+  name = '';
+  email = '';
 
   render() {
     return (
       <div>
         <Card title="Edit student">
           <Form.Label>Name:</Form.Label>
-          <Form.Input
-            type="text"
-            value={this.name}
-            onChange={event => (this.name = event.target.value)}
-          />
+          <Form.Input type="text" value={this.name} onChange={event => (this.name = event.target.value)} />
           <Form.Label>Email:</Form.Label>
-          <Form.Input
-            type="text"
-            value={this.email}
-            onChange={event => (this.email = event.target.value)}
-          />
+          <Form.Input type="text" value={this.email} onChange={event => (this.email = event.target.value)} />
         </Card>
         <Row>
           <Column>
@@ -170,18 +156,13 @@ class StudentNew extends Component {
   }
 
   save() {
-    studentService.newStudent(
-      this.props.match.params.id,
-      this.name,
-      this.email,
-      () => {
-        history.push("/students");
-      }
-    );
+    studentService.newStudent(this.props.match.params.id, this.name, this.email, () => {
+      history.push('/students');
+    });
   }
 
   cancel() {
-    history.push("/students/" + this.props.match.params.id);
+    history.push('/students/' + this.props.match.params.id);
   }
 }
 
@@ -193,7 +174,7 @@ class SubjectList extends Component {
       <Card title="Subjects">
         <List>
           {this.subjects.map(subject => (
-            <List.Item key={subject.id} to={"/subjects/" + subject.id}>
+            <List.Item key={subject.id} to={'/subjects/' + subject.id}>
               {subject.name}
             </List.Item>
           ))}
@@ -239,7 +220,7 @@ class SubjectDetails extends Component {
   }
 
   edit() {
-    history.push("/subjects/" + this.subject.id + "/edit");
+    history.push('/subjects/' + this.subject.id + '/edit');
   }
 }
 
@@ -253,17 +234,9 @@ class SubjectEdit extends Component {
       <div>
         <Card title="Edit subject">
           <Form.Label>Name:</Form.Label>
-          <Form.Input
-            type="text"
-            value={this.subject.name}
-            onChange={e => (this.subject.name = e.target.value)}
-          />
+          <Form.Input type="text" value={this.subject.name} onChange={e => (this.subject.name = e.target.value)} />
           <Form.Label>Subject id:</Form.Label>
-          <Form.Input
-            type="text"
-            value={this.subject.email}
-            onChange={e => (this.subject.email = e.target.value)}
-          />
+          <Form.Input type="text" value={this.subject.email} onChange={e => (this.subject.email = e.target.value)} />
         </Card>
         <Row>
           <Column>
@@ -285,35 +258,27 @@ class SubjectEdit extends Component {
 
   save() {
     studentService.updateSubject(this.subject, () => {
-      history.push("/subjects/" + this.props.match.params.id);
+      history.push('/subjects/' + this.props.match.params.id);
     });
   }
 
   cancel() {
-    history.push("/subjects/" + this.props.match.params.id);
+    history.push('/subjects/' + this.props.match.params.id);
   }
 }
 
 class SubjectNew extends Component {
-  name = "";
-  subid = "";
+  name = '';
+  subid = '';
 
   render() {
     return (
       <div>
         <Card title="Edit student">
           <Form.Label>Name:</Form.Label>
-          <Form.Input
-            type="text"
-            value={this.name}
-            onChange={event => (this.name = event.target.value)}
-          />
+          <Form.Input type="text" value={this.name} onChange={event => (this.name = event.target.value)} />
           <Form.Label>Subject id:</Form.Label>
-          <Form.Input
-            type="text"
-            value={this.subid}
-            onChange={event => (this.subid = event.target.value)}
-          />
+          <Form.Input type="text" value={this.subid} onChange={event => (this.subid = event.target.value)} />
         </Card>
         <Row>
           <Column>
@@ -328,18 +293,13 @@ class SubjectNew extends Component {
   }
 
   save() {
-    studentService.newStudent(
-      this.props.match.params.id,
-      this.name,
-      this.subid,
-      () => {
-        history.push("/subjects");
-      }
-    );
+    studentService.newStudent(this.props.match.params.id, this.name, this.subid, () => {
+      history.push('/subjects');
+    });
   }
 
   cancel() {
-    history.push("/subjects/" + this.props.match.params.id);
+    history.push('/subjects/' + this.props.match.params.id);
   }
 }
 
@@ -358,5 +318,5 @@ ReactDOM.render(
       <Route path="/newsubject" component={SubjectNew} />
     </div>
   </HashRouter>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
