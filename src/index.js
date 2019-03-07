2bbc9f3ec5b5e9ca6a3564656a04fcf49fb7de1a
Lukas Grenstad <lukasgrenstad@gmail.com>
2019-03-07 09:40:36 +0100

    YOLO

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Component } from 'react-simplified';
 import ReactDOM from 'react-dom';
 import { NavLink, HashRouter, Route } from 'react-router-dom';
-import { studentService } from './services';
+import { customerService, bikeService } from './services';
 import { Card, List, Row, Column, NavBar, Button, Form } from './widgets';
 
 import createHashHistory from 'history/createHashHistory';
@@ -11,13 +11,9 @@ const history = createHashHistory(); // Use history.push(...) to programmaticall
 class Menu extends Component {
   render() {
     return (
-      <NavBar brand="AS Sykkelutleie">
-        <NavBar.Link to="/students">Bookings</NavBar.Link>
-        <NavBar.Link to="/newstudent">Customers</NavBar.Link>
-        <NavBar.Link to="/subjects">Bikes</NavBar.Link>
-        <NavBar.Link to="/newsubject">Maintenance</NavBar.Link>
-        <NavBar.Link to="/newsubject">Transportation</NavBar.Link>
-        <NavBar.Link to="/newsubject">Reports</NavBar.Link>
+      <NavBar brand="AS SykkelUtleie">
+        <NavBar.Link to="/customers">Customers</NavBar.Link>
+        <NavBar.Link to="/bikes">Bikes</NavBar.Link>
       </NavBar>
     );
   }
@@ -25,20 +21,19 @@ class Menu extends Component {
 
 class Home extends Component {
   render() {
-    return <Card title="AS Sykkelutleie">Velkommen til AS Sykkelutleie</Card>;
+    return <Card title="Welcome">Welcome to RallRall</Card>;
   }
 }
 
-class StudentList extends Component {
-  students = [];
-
+class Customers extends Component {
+  customers = [];
   render() {
     return (
-      <Card title="Bookings">
+      <Card title="Customers">
         <List>
-          {this.students.map(student => (
-            <List.Item key={student.id} to={'/students/' + student.id}>
-              {student.name}
+          {this.customers.map(customer => (
+            <List.Item key={customer.customerID} to={'/customers/' + customer.customerID}>
+              {customer.FirstName}
             </List.Item>
           ))}
         </List>
@@ -47,275 +42,99 @@ class StudentList extends Component {
   }
 
   mounted() {
-    studentService.getStudents(students => {
-      this.students = students;
+    customerService.getCustomers(customers => {
+      this.customers = customers;
     });
   }
 }
 
-class StudentDetails extends Component {
-  student = null;
+class CustomerDetails extends Component {
+  customer = [];
 
   render() {
-    if (!this.student) return null;
+    if (!this.customer) return null;
 
     return (
       <div>
-        <Card title="Booking">
+        <Card title="Customer details">
           <Row>
-            <Column width={2}>Name:</Column>
-            <Column>{this.student.name}</Column>
+            <Column width={2}>CustomerID:</Column>
+            <Column>{this.customer.CustomerID}</Column>
+          </Row>
+          <Row>
+            <Column width={2}>FirstName:</Column>
+            <Column>{this.customer.FirstName}</Column>
+          </Row>
+          <Row>
+            <Column width={2}>LastName:</Column>
+            <Column>{this.customer.LastName}</Column>
+          </Row>
+          <Row>
+            <Column width={2}>Mobile:</Column>
+            <Column> {this.customer.Mobile}</Column>
           </Row>
           <Row>
             <Column width={2}>Email:</Column>
-            <Column>{this.student.email}</Column>
+            <Column>{this.customer.Email}</Column>
+          </Row>
+          <Row>
+            <Column width={2}>Address:</Column>
+            <Column>{this.customer.Address}</Column>
+          </Row>
+          <Row>
+            <Column width={2}>Zip:</Column>
+            <Column>{this.customer.Zip}</Column>
+          </Row>
+          <Row>
+            <Column width={2}>City:</Column>
+            <Column>{this.customer.City}</Column>
+          </Row>
+          <Row>
+            <Column width={2}>Country:</Column>
+            <Column>{this.customer.Country}</Column>
           </Row>
         </Card>
         <Button.Light onClick={this.edit}>Edit</Button.Light>
       </div>
     );
   }
-
   mounted() {
-    studentService.getStudent(this.props.match.params.id, student => {
-      this.student = student;
-    });
-  }
-
-  edit() {
-    history.push('/students/' + this.student.id + '/edit');
-  }
-}
-
-class StudentEdit extends Component {
-  student = null;
-
-  render() {
-    if (!this.student) return null;
-
-    return (
-      <div>
-        <Card title="">
-          <Form.Label>Name:</Form.Label>
-          <Form.Input type="text" value={this.student.name} onChange={e => (this.student.name = e.target.value)} />
-          <Form.Label>Email:</Form.Label>
-          <Form.Input type="text" value={this.student.email} onChange={e => (this.student.email = e.target.value)} />
-        </Card>
-        <Row>
-          <Column>
-            <Button.Success onClick={this.save}>Save</Button.Success>
-          </Column>
-          <Column right>
-            <Button.Light onClick={this.cancel}>Cancel</Button.Light>
-          </Column>
-        </Row>
-      </div>
-    );
-  }
-
-  mounted() {
-    studentService.getStudent(this.props.match.params.id, student => {
-      this.student = student;
-    });
-  }
-
-  save() {
-    studentService.updateStudent(this.student, () => {
-      history.push('/students/' + this.props.match.params.id);
-    });
-  }
-
-  cancel() {
-    history.push('/students/' + this.props.match.params.id);
-  }
-}
-
-class StudentNew extends Component {
-  name = '';
-  email = '';
-
-  render() {
-    return (
-      <div>
-        <Card title="Customers">
-          <Form.Label>Name:</Form.Label>
-          <Form.Input type="text" value={this.name} onChange={event => (this.name = event.target.value)} />
-          <Form.Label>Email:</Form.Label>
-          <Form.Input type="text" value={this.email} onChange={event => (this.email = event.target.value)} />
-        </Card>
-        <Row>
-          <Column>
-            <Button.Success onClick={this.save}>Save</Button.Success>
-          </Column>
-          <Column right>
-            <Button.Light onClick={this.cancel}>Cancel</Button.Light>
-          </Column>
-        </Row>
-      </div>
-    );
-  }
-
-  save() {
-    studentService.newStudent(this.props.match.params.id, this.name, this.email, () => {
-      history.push('/students');
-    });
-  }
-
-  cancel() {
-    history.push('/students/' + this.props.match.params.id);
-  }
-}
-
-class SubjectList extends Component {
-  subjects = [];
-
-  render() {
-    return (
-      <Card title="Bikes">
-        <List>
-          {this.subjects.map(subject => (
-            <List.Item key={subject.id} to={'/subjects/' + subject.id}>
-              {subject.name}
-            </List.Item>
-          ))}
-        </List>
-      </Card>
-    );
-  }
-
-  mounted() {
-    studentService.getSubjects(subjects => {
-      this.subjects = subjects;
+    customerService.getCustomer(this.props.match.params.id, customer => {
+      this.customer = customer;
     });
   }
 }
-
-class SubjectDetails extends Component {
-  subject = null;
-
-  render() {
-    if (!this.subject) return null;
-
-    return (
-      <div>
-        <Card title="Bike">
-          <Row>
-            <Column width={2}>Name:</Column>
-            <Column>{this.subject.name}</Column>
-          </Row>
-          <Row>
-            <Column width={2}>Subject id:</Column>
-            <Column>{this.subject.subid}</Column>
-          </Row>
-        </Card>
-        <Button.Light onClick={this.edit}>Edit</Button.Light>
-      </div>
-    );
-  }
-
-  mounted() {
-    studentService.getSubject(this.props.match.params.id, subject => {
-      this.subject = subject;
-    });
-  }
-
-  edit() {
-    history.push('/subjects/' + this.subject.id + '/edit');
-  }
-}
-
-class SubjectEdit extends Component {
-  subject = null;
-
-  render() {
-    if (!this.subject) return null;
-
-    return (
-      <div>
-        <Card title="Edit subject">
-          <Form.Label>Name:</Form.Label>
-          <Form.Input type="text" value={this.subject.name} onChange={e => (this.subject.name = e.target.value)} />
-          <Form.Label>Subject id:</Form.Label>
-          <Form.Input type="text" value={this.subject.email} onChange={e => (this.subject.email = e.target.value)} />
-        </Card>
-        <Row>
-          <Column>
-            <Button.Success onClick={this.save}>Save</Button.Success>
-          </Column>
-          <Column right>
-            <Button.Light onClick={this.cancel}>Cancel</Button.Light>
-          </Column>
-        </Row>
-      </div>
-    );
-  }
-
-  mounted() {
-    studentService.getSubject(this.props.match.params.id, subject => {
-      this.subject = subject;
-    });
-  }
-
-  save() {
-    studentService.updateSubject(this.subject, () => {
-      history.push('/subjects/' + this.props.match.params.id);
-    });
-  }
-
-  cancel() {
-    history.push('/subjects/' + this.props.match.params.id);
-  }
-}
-
-class SubjectNew extends Component {
-  name = '';
-  subid = '';
-
-  render() {
-    return (
-      <div>
-        <Card title="Edit student">
-          <Form.Label>Name:</Form.Label>
-          <Form.Input type="text" value={this.name} onChange={event => (this.name = event.target.value)} />
-          <Form.Label>Subject id:</Form.Label>
-          <Form.Input type="text" value={this.subid} onChange={event => (this.subid = event.target.value)} />
-        </Card>
-        <Row>
-          <Column>
-            <Button.Success onClick={this.save}>Save</Button.Success>
-          </Column>
-          <Column right>
-            <Button.Light onClick={this.cancel}>Cancel</Button.Light>
-          </Column>
-        </Row>
-      </div>
-    );
-  }
-
-  save() {
-    studentService.newStudent(this.props.match.params.id, this.name, this.subid, () => {
-      history.push('/subjects');
-    });
-  }
-
-  cancel() {
-    history.push('/subjects/' + this.props.match.params.id);
-  }
-}
+//
+// class Bikes extends Component {
+//   bikes = [];
+//   render() {
+//     return (
+//       <Card title="Bikes">
+//         <List>
+//           {this.bikes.map(bike => (
+//             <List.Item key={bike.bikeID} to={'/bikes/' + bike.bikeID}>
+//               {bike.BikeID}
+//             </List.Item>
+//           ))}
+//         </List>
+//       </Card>
+//     );
+//   }
+//
+//   mounted() {
+//     bikeService.getBikes(bikes => {
+//       this.bikes = bikes;
+//     });
+//   }
+// }
 
 ReactDOM.render(
   <HashRouter>
     <div>
       <Menu />
       <Route exact path="/" component={Home} />
-      <Route exact path="/students" component={StudentList} />
-      <Route exact path="/students/:id" component={StudentDetails} />
-      <Route exact path="/students/:id/edit" component={StudentEdit} />
-      <Route path="/newstudent" component={StudentNew} />
-      <Route exact path="/subjects" component={SubjectList} />
-      <Route exact path="/subjects/:id" component={SubjectDetails} />
-      <Route exact path="/subjects/:id/edit" component={SubjectEdit} />
-      <Route path="/newsubject" component={SubjectNew} />
+      <Route exact path="/customers" component={Customers} />
+      <Route exact path="/customers/:id" component={CustomerDetails} />
     </div>
   </HashRouter>,
   document.getElementById('root')
