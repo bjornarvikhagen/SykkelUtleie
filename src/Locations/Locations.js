8af6a43b5c561ee9afe4359e783424574ffa3110
Bjørnar <bjornar.vikhagen@icloud.com>
2019-04-10 01:49:06 +0200

    mother fucker

--- src/Locations/Locations.js
+++ src/Locations/Locations.js
@@ -22,7 +22,7 @@ export default class Locations extends Component {
               <List>
                 {this.bikes1.map(bike1 => (
                   <List.Item key={bike1.BikeID}>
-                    <input type="checkbox" checked={bike1.checked} onChange={e => bike1.checked = e.target.checked}/>{bike1.BikeID} - {bike1.Brand}''
+                    <input type="checkbox" checked={bike1.checked} onChange={e => bike1.checked = e.target.checked}/> {bike1.BikeID} - {bike1.Brand}
                   </List.Item>
                 ))}
               </List>
@@ -32,12 +32,13 @@ export default class Locations extends Component {
               <p>2 - Finse</p>
               <List>
                 {this.bikes2.map(bike2 => (
-                  <List.Item key={bike2.BikeID} to={'/bikes/' + bike2.BikeID}>
-                    {bike2.Brand} - {bike2.Framesize}''
+                  <List.Item key={bike2.BikeID}>
+                    <input type="checkbox" checked={bike2.checked} onChange={e => bike2.checked = e.target.checked}/> {bike2.BikeID} - {bike2.Brand}
                   </List.Item>
                 ))}
               </List>
             </Column>
+              <Button.Success onClick={() => console.log(this.bikes2)}>test</Button.Success>
             <Column>
               <p>3 - Flåm</p>
               <List>
@@ -68,11 +69,20 @@ export default class Locations extends Component {
                 ))}
               </List>
             </Column>
+            <select id="DropOff" value={this.FK_DropoffID} onChange={e => (this.FK_DropoffID = e.target.value)}>
+  <option value={0}>Choose location..</option>
+  <option value={1}>Haugastøl</option>
+  <option value={2}>Finse </option>
+  <option value={3}>Flåm </option>
+  <option value={4}>Voss </option>
+  <option value={5}>Myrdal </option>
+</select>
           </Row>
         </Card>
       </div>
     );
   }
+
 
   mounted() {
     locationService.getBikesLocation1(bikes1 => {
@@ -80,15 +90,19 @@ export default class Locations extends Component {
       this.bikes1 = bikes1;
     });
     locationService.getBikesLocation2(bikes2 => {
+      for(let bike of bikes2) bike.checked = false;
       this.bikes2 = bikes2;
     });
     locationService.getBikesLocation3(bikes3 => {
+      for(let bike of bikes3) bike.checked = false;
       this.bikes3 = bikes3;
     });
     locationService.getBikesLocation4(bikes4 => {
+      for(let bike of bikes4) bike.checked = false;
       this.bikes4 = bikes4;
     });
     locationService.getBikesLocation5(bikes5 => {
+      for(let bike of bikes5) bike.checked = false;
       this.bikes5 = bikes5;
     });
   }
