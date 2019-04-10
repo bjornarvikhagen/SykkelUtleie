2f325975dcc296ec400c5beaf9338aea6237c759
Lukas Grenstad <lukasgrenstad@gmail.com>
2019-04-10 23:24:52 +0200

    Oppsett av Locations endret. Mangler metode for knappene

--- src/Locations/Locations.js
+++ src/Locations/Locations.js
@@ -22,29 +22,30 @@ export default class Locations extends Component {
               <List>
                 {this.bikes1.map(bike1 => (
                   <List.Item key={bike1.BikeID}>
-                    <input type="checkbox" checked={bike1.checked} onChange={e => bike1.checked = e.target.checked}/> {bike1.BikeID} - {bike1.Brand}
+                    <input type="checkbox" checked={bike1.checked} onChange={e => (bike1.checked = e.target.checked)} />{' '}
+                    {bike1.BikeID} - {bike1.Brand}
                   </List.Item>
                 ))}
               </List>
             </Column>
-            <Button.Success onClick={() => console.log(this.bikes1)}>test</Button.Success>
             <Column>
               <p>2 - Finse</p>
               <List>
                 {this.bikes2.map(bike2 => (
                   <List.Item key={bike2.BikeID}>
-                    <input type="checkbox" checked={bike2.checked} onChange={e => bike2.checked = e.target.checked}/> {bike2.BikeID} - {bike2.Brand}
+                    <input type="checkbox" checked={bike2.checked} onChange={e => (bike2.checked = e.target.checked)} />
+                    {bike2.BikeID} - {bike2.Brand}
                   </List.Item>
                 ))}
               </List>
             </Column>
-              <Button.Success onClick={() => console.log(this.bikes2)}>test</Button.Success>
             <Column>
               <p>3 - Flåm</p>
               <List>
                 {this.bikes3.map(bike3 => (
-                  <List.Item key={bike3.BikeID} to={'/bikes/' + bike3.BikeID}>
-                    {bike3.Brand} - {bike3.Framesize}''
+                  <List.Item key={bike3.BikeID}>
+                    <input type="checkbox" checked={bike3.checked} onChange={e => (bike3.checked = e.target.checked)} />
+                    {bike3.BikeID} - {bike3.Brand}
                   </List.Item>
                 ))}
               </List>
@@ -53,8 +54,9 @@ export default class Locations extends Component {
               <p>4 - Voss</p>
               <List>
                 {this.bikes4.map(bike4 => (
-                  <List.Item key={bike4.BikeID} to={'/bikes/' + bike4.BikeID}>
-                    {bike4.Brand} - {bike4.Framesize}''
+                  <List.Item key={bike4.BikeID}>
+                    <input type="checkbox" checked={bike4.checked} onChange={e => (bike4.checked = e.target.checked)} />
+                    {bike4.BikeID} - {bike4.Brand}
                   </List.Item>
                 ))}
               </List>
@@ -63,48 +65,106 @@ export default class Locations extends Component {
               <p>5 - Myrdal</p>
               <List>
                 {this.bikes5.map(bike5 => (
-                  <List.Item key={bike5.BikeID} to={'/bikes/' + bike5.BikeID}>
-                    {bike5.Brand} - {bike5.Framesize}''
+                  <List.Item key={bike5.BikeID}>
+                    <input type="checkbox" checked={bike5.checked} onChange={e => (bike5.checked = e.target.checked)} />
+                    {bike5.BikeID} - {bike5.Brand}
                   </List.Item>
                 ))}
               </List>
             </Column>
-            <select id="DropOff" value={this.FK_DropoffID} onChange={e => (this.FK_DropoffID = e.target.value)}>
-  <option value={0}>Choose location..</option>
-  <option value={1}>Haugastøl</option>
-  <option value={2}>Finse </option>
-  <option value={3}>Flåm </option>
-  <option value={4}>Voss </option>
-  <option value={5}>Myrdal </option>
-</select>
+          </Row>
+          <Row>
+            <Column>
+              <select id="DropOff" value={this.FK_DropoffID} onChange={e => (this.FK_DropoffID = e.target.value)}>
+                <option value={0}>Choose location..</option>
+                <option value={1}>Haugastøl</option>
+                <option value={2}>Finse </option>
+                <option value={3}>Flåm </option>
+                <option value={4}>Voss </option>
+                <option value={5}>Myrdal </option>
+              </select>
+              <br />
+              <br />
+              <Button.Success onClick={this.sendBikes1}>Send to location</Button.Success>
+            </Column>
+            <Column>
+              <select id="DropOff" value={this.FK_DropoffID} onChange={e => (this.FK_DropoffID = e.target.value)}>
+                <option value={0}>Choose location..</option>
+                <option value={1}>Haugastøl</option>
+                <option value={2}>Finse </option>
+                <option value={3}>Flåm </option>
+                <option value={4}>Voss </option>
+                <option value={5}>Myrdal </option>
+              </select>
+              <br />
+              <br />
+              <Button.Success onClick={this.sendBikes2}>Send to location</Button.Success>
+            </Column>
+            <Column>
+              <select id="DropOff" value={this.FK_DropoffID} onChange={e => (this.FK_DropoffID = e.target.value)}>
+                <option value={0}>Choose location..</option>
+                <option value={1}>Haugastøl</option>
+                <option value={2}>Finse </option>
+                <option value={3}>Flåm </option>
+                <option value={4}>Voss </option>
+                <option value={5}>Myrdal </option>
+              </select>
+              <br />
+              <br />
+              <Button.Success onClick={this.sendBikes3}>Send to location</Button.Success>
+            </Column>
+            <Column>
+              <select id="DropOff" value={this.FK_DropoffID} onChange={e => (this.FK_DropoffID = e.target.value)}>
+                <option value={0}>Choose location..</option>
+                <option value={1}>Haugastøl</option>
+                <option value={2}>Finse </option>
+                <option value={3}>Flåm </option>
+                <option value={4}>Voss </option>
+                <option value={5}>Myrdal </option>
+              </select>
+              <br />
+              <br />
+              <Button.Success onClick={this.sendBikes4}>Send to location</Button.Success>
+            </Column>
+            <Column>
+              <select id="DropOff" value={this.FK_DropoffID} onChange={e => (this.FK_DropoffID = e.target.value)}>
+                <option value={0}>Choose location..</option>
+                <option value={1}>Haugastøl</option>
+                <option value={2}>Finse </option>
+                <option value={3}>Flåm </option>
+                <option value={4}>Voss </option>
+                <option value={5}>Myrdal </option>
+              </select>
+              <br />
+              <br />
+              <Button.Success onClick={this.sendBikes5}>Send to location</Button.Success>
+            </Column>
           </Row>
         </Card>
       </div>
     );
   }
 
-
   mounted() {
     locationService.getBikesLocation1(bikes1 => {
-      for(let bike of bikes1) bike.checked = false;
+      for (let bike of bikes1) bike.checked = false;
       this.bikes1 = bikes1;
     });
     locationService.getBikesLocation2(bikes2 => {
-      for(let bike of bikes2) bike.checked = false;
+      for (let bike of bikes2) bike.checked = false;
       this.bikes2 = bikes2;
     });
     locationService.getBikesLocation3(bikes3 => {
-      for(let bike of bikes3) bike.checked = false;
+      for (let bike of bikes3) bike.checked = false;
       this.bikes3 = bikes3;
     });
     locationService.getBikesLocation4(bikes4 => {
-      for(let bike of bikes4) bike.checked = false;
+      for (let bike of bikes4) bike.checked = false;
       this.bikes4 = bikes4;
     });
     locationService.getBikesLocation5(bikes5 => {
-      for(let bike of bikes5) bike.checked = false;
+      for (let bike of bikes5) bike.checked = false;
       this.bikes5 = bikes5;
     });
   }
-
 }
